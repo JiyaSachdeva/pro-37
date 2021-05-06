f821dc815654d7be0c8f323dd9d9a96ebceceaba
JiyaSachdeva <74546515+JiyaSachdeva@users.noreply.github.com>
2021-05-06 22:26:38 +0530

    Add files via upload

---
+++ Quiz.js
@@ -0,0 +1,81 @@
+class Quiz {
+  constructor(){}
+
+  getState(){
+    var gameStateRef  = database.ref('gameState');
+    gameStateRef.on("value",function(data){
+       gameState = data.val();
+    })
+
+  }
+
+  update(state){
+    database.ref('/').update({
+      gameState: state
+    });
+  }
+
+  async start(){
+    if(gameState === 0){
+      contestant = new Contestant();
+      var contestantCountRef = await database.ref('contestantCount').once("value");
+      if(contestantCountRef.exists()){
+        contestantCount = contestantCountRef.val();
+        contestant.getCount();
+      }
+      question = new Question()
+      question.display();
+    }
+  }
+
+  play(){
+    //write code here to hide question elements
+    background("Yellow");
+    question.hide();
+    fill ("red");
+    textSize (20);
+    text ("RESULT OF THE QUIZ",340,65);
+
+    Contestant.getPlayerInfo();
+    //write code to change the background color here
+    if (allContestants!== undefined){
+      
+      fill ("Blue");
+      textSize (20);
+      text("*NOTE: Contestant who answered correct are highlighted in green colour!!",130,230);
+      var display_Answer = 230;
+      
+      for (var plr in allContestants){
+        var correctAns = "2";
+
+        if(correctAns===allContestants[plr].answer){
+          fill ("Green");
+        }
+        else
+        fill ("red");
+       
+         display_Answer=+30;
+        textSize (20);
+        text(allContestants[plr].name + ":" + allContestants[plr].answer,250,display_Answer);
+        console.log(display_Answer);
+      }
+
+
+
+
+    }
+
+    //write code to show a heading for showing the result of Quiz
+
+    //call getContestantInfo( ) here
+
+
+    //write condition to check if contestantInfor is not undefined
+
+    //write code to add a note here
+
+    //write code to highlight contest who answered correctly
+    
+  }
+
+}
